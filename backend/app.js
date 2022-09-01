44c6c9abe264ff28d293d667be91a213b998d26c
Rosine <kabore.rosine75@gmail.com>
2022-09-01 11:29:49 +0200

    creation utilisateur

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
-const app = express();
-
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/user');
+const path = require('path');
 
 mongoose.connect('mongodb+srv://Rosizou:<Leslie75018>@cluster0.qoxtth6.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -10,8 +11,18 @@ mongoose.connect('mongodb+srv://Rosizou:<Leslie75018>@cluster0.qoxtth6.mongodb.n
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
 });
+
+app.use(bodyParser.json());
+
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
