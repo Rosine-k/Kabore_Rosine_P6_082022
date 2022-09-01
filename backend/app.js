01b9ec200af7e99001e20d115a81f8bb10d831a3
Rosine <kabore.rosine75@gmail.com>
2022-09-01 12:04:36 +0200

    middleware et sauce

--- backend/app.js
+++ backend/app.js
@@ -22,6 +22,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
+app.use('/api/sauces', stuffRoutes);
 app.use('/api/auth', userRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
