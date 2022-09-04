40947a3141261594fc3ead069206996092b71df0
Rosine <kabore.rosine75@gmail.com>
2022-09-04 18:53:13 +0200

    modifs erreurs

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,27 +1,27 @@
-const Thing = require('../models/sauce');
+const Sauce = require('../models/sauce');
 const fs = require('fs');
 
 exports.createThing = (req, res, next) => {
   const thingObject = JSON.parse(req.body.thing);
   delete thingObject._id;
   delete thingObject._userId;
-  const thing = new Thing({
+  const sauce = new Thing({
       ...thingObject,
       userId: req.auth.userId,
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
   });
 
-  thing.save()
+  sauce.save()
   .then(() => { res.status(201).json({message: 'Objet enregistré !'})})
   .catch(error => { res.status(400).json( { error })})
 };
 
 exports.getOneThing = (req, res, next) => {
-  Thing.findOne({
+  Sauce.findOne({
     _id: req.params.id
   }).then(
-    (thing) => {
-      res.status(200).json(thing);
+    (sauce) => {
+      res.status(200).json(sauce);
     }
   ).catch(
     (error) => {
@@ -34,17 +34,17 @@ exports.getOneThing = (req, res, next) => {
 
 exports.modifyThing = (req, res, next) => {
   const thingObject = req.file ? {
-      ...JSON.parse(req.body.thing),
+      ...JSON.parse(req.body.sauce),
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
   } : { ...req.body };
 
   delete thingObject._userId;
-  Thing.findOne({_id: req.params.id})
-      .then((thing) => {
-          if (thing.userId != req.auth.userId) {
+  Sauce.findOne({_id: req.params.id})
+      .then((sauce) => {
+          if (sauce.userId != req.auth.userId) {
               res.status(401).json({ message : 'Not authorized'});
           } else {
-              Thing.updateOne({ _id: req.params.id}, { ...thingObject, _id: req.params.id})
+              Sauce.updateOne({ _id: req.params.id}, { ...thingObject, _id: req.params.id})
               .then(() => res.status(200).json({message : 'Objet modifié!'}))
               .catch(error => res.status(401).json({ error }));
           }
@@ -55,14 +55,14 @@ exports.modifyThing = (req, res, next) => {
 };
 
 exports.deleteThing = (req, res, next) => {
-  Thing.findOne({ _id: req.params.id})
-      .then(thing => {
-          if (thing.userId != req.auth.userId) {
+  Sauce.findOne({ _id: req.params.id})
+      .then(sauce => {
+          if (sauce.userId != req.auth.userId) {
               res.status(401).json({message: 'Not authorized'});
           } else {
-              const filename = thing.imageUrl.split('/images/')[1];
+              const filename = sauce.imageUrl.split('/images/')[1];
               fs.unlink(`images/${filename}`, () => {
-                  Thing.deleteOne({_id: req.params.id})
+                  Sauce.deleteOne({_id: req.params.id})
                       .then(() => { res.status(200).json({message: 'Objet supprimé !'})})
                       .catch(error => res.status(401).json({ error }));
               });
@@ -74,9 +74,9 @@ exports.deleteThing = (req, res, next) => {
 };
 
 exports.getAllStuff = (req, res, next) => {
-  Thing.find().then(
-    (things) => {
-      res.status(200).json(things);
+  Sauce.find().then(
+    (sauces) => {
+      res.status(200).json(sauces);
     }
   ).catch(
     (error) => {
--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -4,12 +4,12 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
-const stuffCtrl = require('../controllers/sauces');
+const saucesCtrl = require('../controllers/sauces');
 
-router.get('/', auth, stuffCtrl.getAllStuff);
-router.post('/', auth, multer, stuffCtrl.createThing);
-router.get('/:id', auth, stuffCtrl.getOneThing);
-router.put('/:id', auth, multer, stuffCtrl.modifyThing);
-router.delete('/:id', auth, stuffCtrl.deleteThing);
+router.get('/', auth, saucesCtrl.getAllStuff);
+router.post('/', auth, multer, saucesCtrl.createThing);
+router.get('/:id', auth, saucesCtrl.getOneThing);
+router.put('/:id', auth, multer, saucesCtrl.modifyThing);
+router.delete('/:id', auth, saucesCtrl.deleteThing);
 
 module.exports = router;
